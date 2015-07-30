81bbe09f59ce019d20bbe77ca345159033e5346a
rwb <robert.w.bliss@gmail.com>
2015-07-30 12:00:16 -0400

    Added diagnostic messages for the long waits...

--- schedule_test.py
+++ schedule_test.py
@@ -24,10 +24,11 @@ password = getpass("pass: ", stream=sys.stderr)
 
 web = wa.WebAdvisor("https://oasis.oglethorpe.edu")
 web.follow_link("Log In")
+print("This may take a few seconds...", file=sys.stderr)
+print("Logging in...", file=sys.stderr)
 web.login(user, password)
 web.follow_link("for Students")
 web.follow_link("My class")
 web.get_class_schedule("FA15R")
-
-print("Schedule for %s:" % user)
+print("Grabbing schedule...", file=sys.stderr)
 wa.print_with_args(args, web.grab_schedule_rows(web.last_request, args.verbose))
