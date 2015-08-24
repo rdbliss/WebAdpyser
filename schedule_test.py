438e2470e2687cf153cbb219f8497fe68060c90a
rwb <robert.w.bliss@gmail.com>
2015-08-24 11:25:33 -0400

    Cleaning spacing, removed unused bs4; you're welcome, pylint

--- schedule_test.py
+++ schedule_test.py
@@ -1,14 +1,13 @@
 #!/usr/bin/python3
 import wa
-from bs4 import BeautifulSoup
 from getpass import getpass
 import argparse
 import sys
 
 desc = "Grab a course schedule from OASIS."
-epilog=("Grabbing schedules is a little slower than grabbing course listings "
-        "because to get all of the 'base' information, we have to GET the "
-        "whole class page, not just the table summarizing the info.")
+epilog = ("Grabbing schedules is a little slower than grabbing course listings "
+          "because to get all of the 'base' information, we have to GET the "
+          "whole class page, not just the table summarizing the info.")
 
 parser = argparse.ArgumentParser(description=desc, epilog=epilog)
 wa.add_filter_args(parser)
@@ -32,4 +31,5 @@ if not web.login(user, password):
 web.follow_link("for Students")
 web.follow_link("My class")
 web.get_class_schedule(args.term)
-wa.print_with_args(args, web.grab_schedule_rows(web.last_request, not args.ignore_faculty))
+wa.print_with_args(args, web.grab_schedule_rows(web.last_request,
+                                                not args.ignore_faculty))
